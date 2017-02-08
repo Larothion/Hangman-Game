b2ba813d341c05827d47c992b0f3bf27c98fc401
Taylor Harvet <larothion@aol.com>
2017-02-07 23:14:47 -0500

    2nd commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 
 var guessesLeft = 10;
+document.getElementById("guess-Remaining").innerHTML = guessesLeft;
 var losses = 0;
 var wins = 0;
 var previousGuesses = [];
+var goodGuess=false;
 
 /*Create Array of possible words, have the computer choose one at random.*/
 var wordsArray = ["flyleaf", "eagles", "killers"]; 
@@ -10,6 +12,7 @@ var randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 console.log(randomWord);
 var previousGuesses = [];
 var answerArray=[];
+
 
 
 /*Created a variable for answerArray and a "for loop" that creates the amount of underscores needed
@@ -21,7 +24,7 @@ for (var i=0; i < randomWord.length ; i++) {
 console.log(answerArray);
 
 document.getElementById("wordSpace").innerHTML = answerArray.join(" ");
-
+console.log(answerArray);
 
 /*Users start the game by the ".onkeyup" event.  Created a function to store the user's 
 key choice and then, if the key choice is correct, display so in the console. Console
@@ -30,21 +33,47 @@ document.onkeyup = function(event) {
 	var userGuess = event.key;
 
 	
-		if (userGuess !== previousGuesses/*issue*/) {
+		/*if (userGuess !== previousGuesses) {
 			previousGuesses = previousGuesses + userGuess;
 			console.log(previousGuesses);
 		}	else {
 		
-	 		}
+	 		}*/ 
+      for (var i=0; i < randomWord.length ; i++) {
+    if (userGuess[i] === randomWord[i]) {
+      answerArray[i] = userGuess;
+    } else { }
+  }
 	 
 
 
-	for (var i=0; i < randomWord.length ; i++) {
-		if (userGuess == randomWord[i]) {
-			answerArray[i] = userGuess;
-			document.getElementById("wordSpace").innerHTML = answerArray.join(" ");
-		} else { }
-	}
+
+   for(var i = 0; i < randomWord.length; i++) {
+            if(randomWord[i] === userGuess) {
+                answerArray[i] = userGuess;
+                goodGuess = true;
+            }
+        }//end for loop
+
+          if (goodGuess === false) {
+
+          }
+
+for(var i = 0; i < randomWord.length; i++) {
+            if(randomWord[i] === userGuess) {
+                answerArray[i] = userGuess;
+                goodGuess = true;
+                console.log(goodGuess);
+            }
+        }//end for loop
+
+        if(goodGuess === false) {
+            console.log(goodGuess);
+            document.getElementById('guess-Remaining').innerHTML = "Guesses Remaining: " + --guessesLeft;
+        }
+        goodGuess = false;
+
+
 			
 	for (var i=0; i < randomWord.length; i++) {
 
@@ -53,7 +82,28 @@ document.onkeyup = function(event) {
 				isRight = true
 				console.log("You got the letter " + randomWord[i]);	
 		} else  {	}
+
+
+    document.getElementById("wordSpace").innerHTML = answerArray.join(" ");
 	}
+
+ /* keep track of letters remaining to be guess*/
+        var lettersRemaining = randomWord.length;
+        //go through loop to count the remaining letters not guessed
+        for(var i = 0; i < randomWord.length; i++) {
+            if(answerArray[i] !== '_'){
+                lettersRemaining -= 1;
+            }
+        }//end for loop
+        console.log(lettersRemaining);
+
+        //if statement that shows a winning message when the user guesses the correct word
+        //showsElement() is also activated
+        if(lettersRemaining == 0){
+            console.log("NICE!, You guessed the word!");
+            winCounter.innerHTML = "Wins: " + ++wins;
+            /*showButtonElement()*/;
+          }
 };
 
 
